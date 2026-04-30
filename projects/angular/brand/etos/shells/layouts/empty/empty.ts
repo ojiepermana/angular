0ebebabc3cca899b45531f28b9c509d3bfab20ec
ojiepermana <me@ojiepermana.com>
2026-04-30 23:12:33 +0700

    Update Angular deps and refine empty layout

--- projects/angular/brand/etos/shells/layouts/empty/empty.ts
+++ projects/angular/brand/etos/shells/layouts/empty/empty.ts
@@ -6,19 +6,19 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   template: `
     <div
-      class="min-h-screen bg-neutral-200 text-neutral-600 bg-[linear-gradient(rgba(212,212,212,0.45)_1px,transparent_1px),linear-gradient(to_right,rgba(212,212,212,0.45)_1px,transparent_1px)] bg-position-[center_center] bg-size-[2.775rem_2.775rem]">
-      <div class="relative isolate overflow-hidden py-18">
-        <div aria-hidden="true" class="pointer-events-none absolute inset-y-0 inset-x-0 px-20">
-          <div class="relative mx-auto h-full container">
-            <div class="absolute inset-y-0 left-0 w-px bg-neutral-300"></div>
-            <div class="absolute inset-y-0 right-0 w-px bg-neutral-300"></div>
+      class="h-dvh overflow-hidden bg-neutral-200  bg-[linear-gradient(rgba(212,212,212,0.45)_1px,transparent_1px),linear-gradient(to_right,rgba(212,212,212,0.45)_1px,transparent_1px)] bg-position-[center_center] bg-size-[2.775rem_2.775rem]">
+      <div class="relative isolate box-border h-full overflow-hidden py-3">
+        <div aria-hidden="true" class="pointer-events-none absolute inset-y-0 inset-x-0 px-3">
+          <div class="relative mx-auto h-full w-full">
+            <div class="absolute inset-y-0 left-0 w-px bg-brand"></div>
+            <div class="absolute inset-y-0 right-0 w-px bg-brand"></div>
           </div>
         </div>
 
-        <div class="border border-neutral-300 px-20">
-          <div class="mx-auto container flex min-h-[calc(100vh-8rem)] flex-col bg-white">
+        <div class="h-full min-h-0 border border-brand px-3">
+          <main class="mx-auto w-full h-full min-h-0 overflow-y-auto bg-background/65">
             <router-outlet />
-          </div>
+          </main>
         </div>
       </div>
     </div>
